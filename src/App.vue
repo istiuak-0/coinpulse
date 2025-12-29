cc4ff5f8e1806b524ae8abb4f57e1c418f27e2d3
istiuak <istiuak.0@gmail.com>
2025-12-29 11:57:28 +0600

    feat(ui): setup shadcn vue and unhead

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useHead } from '@unhead/vue';
 
-<template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
-
-<style scoped></style>
+useHead({
+  title: 'coinpulse',
+  meta: [{ name: 'description', content: 'Crypto Screener App with built in high Frequency Terminal & Dashboard' }],
+});
+</script>
+<template></template>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import './style.css';
+import App from './App.vue';
+import router from './router';
 
-import App from './App.vue'
-import router from './router'
+import { createHead } from '@unhead/vue/client'
 
-const app = createApp(App)
+const app = createApp(App);
+const head = createHead()
+app.use(head)
 
-app.use(createPinia())
-app.use(router)
 
-app.mount('#app')
+
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
